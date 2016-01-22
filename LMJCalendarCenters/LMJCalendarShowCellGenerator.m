4cf9e3878edbd5f7dd1755e6f3cc15ad42153957
limingjie <limingjie_mail@163.com>
2016-01-22 10:35:36 +0800

    1.0.0

--- LMJCalendarCenters/LMJCalendarShowCellGenerator.m
+++ LMJCalendarCenters/LMJCalendarShowCellGenerator.m
@@ -8,7 +8,11 @@
 //  主要根据提供的item数据生成不同种类的日期单元
 //
 //  Created by MajorLi on 14-11-6.
-//  Copyright (c) 2014年 apple. All rights reserved.
+//  Copyright (c) 2014年 iOS开发者公会. All rights reserved.
+//
+//  iOS开发者公会-技术1群 QQ群号：87440292
+//  iOS开发者公会-技术2群 QQ群号：232702419
+//  iOS开发者公会-议事区  QQ群号：413102158
 //
 
 #import "LMJCalendarShowCellGenerator.h"
--- LMJCalendarModels/LMJCalendarItemModel.m
+++ LMJCalendarModels/LMJCalendarItemModel.m
@@ -7,7 +7,11 @@
 //  数据模型 -- 日期元素 （继承于LMJCalendarDateDayModel）
 //
 //  Created by MajorLi on 14-11-5.
-//  Copyright (c) 2014年 apple. All rights reserved.
+//  Copyright (c) 2014年 iOS开发者公会. All rights reserved.
+//
+//  iOS开发者公会-技术1群 QQ群号：87440292
+//  iOS开发者公会-技术2群 QQ群号：232702419
+//  iOS开发者公会-议事区  QQ群号：413102158
 //
 
 #import "LMJCalendarItemModel.h"
